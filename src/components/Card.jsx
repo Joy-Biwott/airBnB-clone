4e6ca68548797b4c2906351ff03b045441bbb213
Joy <biwott.joy.03@gmail.com>
2023-09-06 23:53:32 +0300

    badge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 function Card (props){
+    let badgeText
+    if (props.openSpots ===0){
+        badgeText="SOLD OUT"
+    } else if (props.location === "Online"){
+        badgeText="ONLINE"
+    }
     return(
         <div className="card">
+           {badgeText && <div className="card--badge">{badgeText}</div>}
            <img className="card--image" src={`../public/assets/${props.coverImg}`} alt={props.coverImg}/> 
            <div className="card--stats">
             <img src="../public/assets/star.png" className="card--star"/>
